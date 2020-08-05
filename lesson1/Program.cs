adb1982d8cf5a2631a835b1ffe74c42426c886c0
thomas m zeng <thomas.zeng@gmail.com>
2020-08-04 18:35:55 -0700

    Code of Lesson1:
    Lookup the System.DateTime structure. This type contains a large amount
    of functionality that allows a user to perform all sorts of interesting
    operations with dates and times. Use System.DateTime along with
    System.Console to implement a # program that does the following:
    
    Ask the user for their birthday. It will probably easiest to ask year,
    then month, then day rather than parsing a combined string reliably.
    Calculate the age of the user.
    Check to see if the age of the user is impossible. For example, if the
    user is not yet born, output an error message. If the user claims to be
    135 years old, then let them know that's not possible.
    
    Task: Zigger-lesson1

---
+++ lesson1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Bday
+    {
+        DateTime birthday;
+        int y;
+        int age;
+
+        public string ToString() {
+            return birthday.ToString();
+        }
+        public void Enter() {
+            Console.WriteLine("Hello, enter the year of your birthday please:");
+            string s = System.Console.ReadLine();
+
+            Console.WriteLine(s);
+            this.y = Convert.ToInt32(s);
+
+            DateTime dat = DateTime.Now;
+
+            if ( y >= dat.Year) {
+                Console.WriteLine("You are not born yet. Error.");
+                return;
+            } else if (y+135 < dat.Year) {
+                Console.WriteLine("You are not honest. How can you be 135 year old, or older!");
+                return;
+            }
+            age = dat.Year - y;
+
+            Console.WriteLine("You are {0:G} years old", age);
+        }
+        static void Main(string[] args)
+        {
+            Bday bday = new Bday();
+
+            DateTime dat = DateTime.Now;
+
+            Console.WriteLine("\nToday is {0:d} at {0:T}.", dat);
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+
+            bday.Enter();
+        }
+    }
+}
