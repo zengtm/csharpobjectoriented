2fc65f3095b5c74551ed54f442a7d9a3ffc4e900
thomas m zeng <thomas.zeng@gmail.com>
2020-08-16 18:01:45 -0700

    Add Jagged Array
    Jira: 2

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -15,10 +15,6 @@ namespace lesson1
             Console.WriteLine("Hello, enter the year of your birthday please:");
             string s = System.Console.ReadLine();
 
-<<<<<<< HEAD
-=======
-            Console.WriteLine(s);
->>>>>>> adb1982d8cf5a2631a835b1ffe74c42426c886c0
             this.y = Convert.ToInt32(s);
 
             DateTime dat = DateTime.Now;
@@ -34,8 +30,35 @@ namespace lesson1
 
             Console.WriteLine("You are {0:G} years old", age);
         }
+
+        static void ShowJagArray() {
+            int[][] jag = new int[3][];
+            jag[0] = new int[5];
+            jag[1] = new int[4];
+            jag[2] = new int[2];
+
+            Console.WriteLine("{0}", jag.ToString());
+
+            for (int i=0; i < jag.GetLength(0); i++)
+            {
+                for (int j=0; j < jag[i].Length; j++)
+                {
+                    Console.Write("\t{0}", jag[i][j]); // would jag[i,j] work?
+                }
+                Console.WriteLine();
+            }
+
+        }
         static void Main(string[] args)
         {
+            int[,] i = {{1,0,0}, {0,1,0}, {0,0,1}};
+
+            int len1 = i.Length;
+
+            Console.WriteLine("len1 is {0}", len1);
+
+            ShowJagArray();
+
             Bday bday = new Bday();
 
             DateTime dat = DateTime.Now;
@@ -45,6 +68,10 @@ namespace lesson1
             Console.ReadLine();
 
             bday.Enter();
+
+            Bday b2 = new Bday();
+
+            bday = b2;
         }
     }
 }
